a527c2bf5d28f419dd1911b4ae898a9b3c40db60
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-09-28 16:12:39 +0300

    listening

--- src/index.js
+++ src/index.js
@@ -3,17 +3,53 @@ const uWS = require("uWebSockets.js");
 
 class WebSocketServer {
     constructor(options = {}, callback) {
-        if(!settings?.uwsOptions) {
-            settings.uwsOptions = {};
+        if(options.noServer) {
+            throw new Error("noServer is not supported. Read documentation for how to properly setup WS server with custom http server.");
+        }
+        if(!options.server && !options.port) {
+            throw new Error("server or port is required");
+        }
+        this.listenCalled = false;
+        if(!options?.uwsOptions) {
+            options.uwsOptions = {};
         }
 
-        if(settings.uwsOptions.key_file_name && settings.uwsOptions.cert_file_name) {
-            this.uwsApp = uWS.SSLApp(settings.uwsOptions);
-            this.ssl = true;
+        if(!options.server) {
+            if(options.uwsOptions.key_file_name && options.uwsOptions.cert_file_name) {
+                this.uwsApp = uWS.SSLApp(options.uwsOptions);
+                this.ssl = true;
+            } else {
+                this.uwsApp = uWS.App(options.uwsOptions);
+                this.ssl = false;
+            }
+            this.listen(options.port, callback);
         } else {
-            this.uwsApp = uWS.App(settings.uwsOptions);
-            this.ssl = false;
+            this.uwsApp = options.server;
         }
+    }
+
+    listen(port, callback) {
+        if(!callback && typeof port === 'function') {
+            callback = port;
+            port = 0;
+        }
+        let fn = 'listen';
+        if(typeof port !== 'number') {
+            if(!isNaN(Number(port))) {
+                port = Number(port);
+            } else {
+                fn = 'listen_unix';
+            }
+        }
+        this.listenCalled = true;
+        this.uwsApp[fn](port, socket => {
+            if(!socket) {
+                let err = new Error('Failed to listen on port ' + port + '. No permission or address already in use.');
+                throw err;
+            }
+            this.port = uWS.us_socket_local_port(socket);
+            if(callback) callback(this.port);
+        });
     }
 }
 
--- tests/tests/ws/listen.js
+++ tests/tests/ws/listen.js
@@ -2,18 +2,10 @@
 
 const ws = require("ws");
 
-const wss = new ws.WebSocketServer({ port: 8080 });
-
-wss.on("connection", (ws) => {
-    ws.on("message", (message) => {
-        console.log(message);
-    });
-});
-
-setTimeout(() => {
+const wss = new ws.WebSocketServer({ port: 8080 }, () => {
     const c = new ws.WebSocket("ws://localhost:8080");
     c.on("open", () => {
         console.log("Connected to server");
-        c.send("Hello from client");
+        process.exit(0);
     });
 }, 500);
